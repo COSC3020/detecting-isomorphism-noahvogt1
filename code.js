d2b6405f772eb1b9662f3a634a65ec3716b7e2f8
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-04-02 17:01:42 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,44 +1,53 @@
-function degree(graph, node) {
-  let degree = 0;
-  for (let i = 0; i < graph[node].length; i++) {
-    if (graph[node][i] === 1) {
-      degree++;
+function generateAllPermutations(array) {
+    if (array.length === 1) return [array];
+    let permutations = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let remainingElements = generateAllPermutations(array.slice(0, i).concat(array.slice(i + 1)));
+        
+        for (let perm of remainingElements) {
+            permutations.push([array[i], ...perm]);
+        }
     }
-  }
-  return degree;
+    return permutations;
 }
 
-function getSortedNodesByDegree(graph) {
-  let nodeDegrees = [];
-  for (let i = 0; i < graph.length; i++) {
-    nodeDegrees.push([degree(graph, i), i]);
-  }
-  nodeDegrees.sort((a, b) => a[0] - b[0]);
-  return nodeDegrees;
+function matricesAreEqual(permutedGraph, graph2) {
+    for (let i = 0; i < permutedGraph.length; i++) {
+        for (let j = 0; j < permutedGraph.length; j++) {
+            if (permutedGraph[i][j] != graph2[i][j]) return false;
+        }
+    }
+    return true;
 }
 
-function areIsomorphic(graph1, graph2) {
-  let sortedNodes1 = getSortedNodesByDegree(graph1);
-  let sortedNodes2 = getSortedNodesByDegree(graph2);
+function checkGraphIsomorphism(graph1, graph2) {
+    if (graph1.length != graph2.length) return false;
 
-  if (sortedNodes1.length !== sortedNodes2.length) {
+    let array = Array(graph1.length);
+    for (let i = 0; i < graph1.length; i++) {
+        array[i] = i;
+    }
+
+    let allPermutations = generateAllPermutations(array);
+
+    for (let permutation of allPermutations) {
+
+        let permutedGraph = Array(graph1.length);
+        for (let i = 0; i < graph1.length; i++) {
+            permutedGraph[i] = Array(graph1.length).fill(0);
+        }
+        
+        for (let i = 0; i < graph1.length; i++) {
+            for (let j = 0; j < graph1.length; j++) {
+                permutedGraph[i][j] = graph1[permutation[i]][permutation[j]];
+            }
+        }
+        
+        if (matricesAreEqual(permutedGraph, graph2)) {
+            return true;
+        }
+    }
+    
     return false;
-  }
-
-  let nodeMap = [];
-  for (let i = 0; i < sortedNodes1.length; i++) {
-    let node1 = sortedNodes1[i][1];
-    let node2 = sortedNodes2[i][1];
-    nodeMap[node1] = node2;
-  }
-
-  for (let i = 0; i < graph1.length; i++) {
-    for (let j = 0; j < graph1.length; j++) {
-      if (graph1[i][j] !== graph2[nodeMap[i]][nodeMap[j]]) {
-        return false;
-      }
-    }
-  }
-
-  return true;
 }
